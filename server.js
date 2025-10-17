6f0d21b3f81e1e686c105c5658830c5a51e57983
Jarol Rovinson Cuarán Gaitán <asecontcieras@hotmail.comm>
2025-10-17 14:33:26 -0500

    Se cambió el puerto

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/userRoutes');
 const favoritosRoutes = require('./routes/favoritosRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middlewares
 app.use(cors());
